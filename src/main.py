0f84b4de180430e2d14cd602459773c655d01036
Ari Archer <ari@ari.lt>
2024-06-02 01:28:32 +0300

    Begin implementation of the AST.
    Signed-off-by: Ari Archer <ari@ari.lt>

--- src/main.py
+++ src/main.py
@@ -4,12 +4,13 @@
 
 import sys
 import typing as t
+from dataclasses import dataclass
+from enum import Enum, auto
 from warnings import filterwarnings as filter_warnings
 
 SPECIAL_TOKENS: t.Final[t.Set[str]] = {
     ";",
     ",",
-    ".",
     "(",
     ")",
     "{",
@@ -20,27 +21,120 @@ SPECIAL_TOKENS: t.Final[t.Set[str]] = {
     "-",
     "*",
     "/",
-    "=",
-    "<",
-    ">",
     "!",
-    "&",
-    "|",
     "^",
     "%",
     "~",
 }
+STRING_PREFIXES: t.Final[str] = "nl"
 
 
-def tokenize(code: str) -> list[str]:
+class TokenType(Enum):
+    type = auto()
+    aname = auto()  # A name.
+    function = auto()
+    string = auto()
+    number = auto()
+    operator = auto()
+    elipsis = auto()
+
+
+@dataclass
+class Token:
+    type: TokenType
+    token: str
+    char: int
+    line: int
+    info: t.Optional[t.Any]
+
+
+class TypeType(Enum):
+    u8 = auto()
+    i8 = auto()
+    char = auto()
+    bool = auto()
+    u16 = auto()
+    i16 = auto()
+    char16 = auto()
+    u32 = auto()
+    i32 = auto()
+    f32 = auto()
+    char32 = auto()
+    u64 = auto()
+    i64 = auto()
+    f64 = auto()
+    ptr = auto()
+    any = auto()
+
+
+@dataclass
+class Type:
+    qualifiers: t.List[Token]
+    type: TypeType
+    pdepth: int  # Pointer depth
+
+
+@dataclass
+class Name:
+    type: Type
+    name: str
+
+
+@dataclass
+class Function:
+    name: Name
+    args: t.List[Name]
+    variadic: bool
+    body: t.List[Token]
+
+
+def log(*args: str) -> None:
+    print(*args)
+
+
+def info(file: str, line: int, char: int, *args: str) -> None:
+    print(f"{file}:{line}:{char}: \033[1m\033[34mInfo:\033[0m", *args)
+
+
+def success(file: str, line: int, char: int, *args: str) -> None:
+    print(f"{file}:{line}:{char}: \033[1m\033[32mSuccess:\033[0m", *args)
+
+
+def warn(file: str, line: int, char: int, *args: str) -> None:
+    print(
+        f"{file}:{line}:{char}: \033[1m\033[33mWarning:\033[0m", *args, file=sys.stderr
+    )
+
+
+def err(file: str, line: int, char: int, *args: str) -> None:
+    print(
+        f"{file}:{line}:{char}: \033[1m\033[31mWarning:\033[0m", *args, file=sys.stderr
+    )
+
+
+def fatal(file: str, line: int, char: int, *args: str) -> t.NoReturn:
+    print(
+        f"{file}:{line}:{char}: \033[1m\033[31mFatal error:\033[0m",
+        *args,
+        file=sys.stderr,
+    )
+    sys.exit(1)
+
+
+def tokenize(code: str) -> t.Tuple[int, int, t.List[t.Tuple[int, int, str]]]:
     """Tokenize given code into a list of tokens, ignoring comments."""
 
     idx: int = 0
-    tokens: list[str] = []
+    tokens: t.List[t.Tuple[int, int, str]] = []
     length: int = len(code)
+    line: int = 1
 
     while idx < length:
         start: int = idx  # Start of the token.
+
+        # Count lines
+        if code[idx] == "\n":
+            line += 1
 
         # Skip whitespace.
         if code[idx].isspace():
@@ -61,16 +155,16 @@ def tokenize(code: str) -> list[str]:
             end_comment: int = code.find("*/", idx + 2)
 
             if end_comment == -1:
-                raise ValueError("Block comment not closed")
+                raise ValueError(f"Block comment not closed at line {line}.")
 
             idx: int = end_comment + 2
             continue
 
-        # Handle strings with(out) prefixes u, l.
+        # Handle strings with(out) prefixes n (non-null, no NULL at the end), l (literal).
         if code[idx] in "'\"" or (
-            code[idx] in "ul" and idx + 1 < length and code[idx + 1] in "'\""
+            code[idx] in STRING_PREFIXES and idx + 1 < length and code[idx + 1] in "'\""
         ):
-            if code[idx] in "ul" and code[idx + 1] in "'\"":
+            if code[idx] in STRING_PREFIXES and code[idx + 1] in "'\"":
                 idx += 2  # Skip prefix and opening quote.
                 end_char = code[idx - 1]
             else:
@@ -86,7 +180,7 @@ def tokenize(code: str) -> list[str]:
             if idx < length:
                 idx += 1  # Skip closing quote
 
-            tokens.append(code[start:idx])
+            tokens.append((line, start, code[start:idx]))
             continue
 
         # Handle identifiers and keywords.
@@ -94,7 +188,7 @@ def tokenize(code: str) -> list[str]:
             while idx < length and (code[idx].isalnum() or code[idx] == "_"):
                 idx += 1
 
-            tokens.append(code[start:idx])
+            tokens.append((line, start, code[start:idx]))
             continue
 
         # Handle numbers (integers, hex, octal, binary, floats).
@@ -125,13 +219,13 @@ def tokenize(code: str) -> list[str]:
                 idx += 1
 
             tokens.append(
-                code[start:idx].replace("_", "")
-            )  # Remove underscores from the token
+                (line, start, code[start:idx].replace("_", ""))
+            )  # Remove underscores from the token.
             continue
 
         # Handle special symbols.
         if code[idx] in SPECIAL_TOKENS:
-            tokens.append(code[idx])
+            tokens.append((line, start, code[idx]))
             idx += 1
             continue
 
@@ -144,9 +238,68 @@ def tokenize(code: str) -> list[str]:
         ):
             idx += 1
 
-        tokens.append(code[start:idx])
+        tokens.append((line, start, code[start:idx]))
 
-    return tokens
+    return line, idx, tokens
+
+
+def build_ast(tokens: t.List[t.Tuple[int, int, str]], file: str) -> t.List[Token]:
+    """Parse given tokens into a list of tokens(?)"""
+
+    ast: t.List[Token] = []
+    idx: int = 0
+
+    type_tokens: t.Set[str] = {typ.name for typ in TypeType}
+
+    while idx < len(tokens):
+        char, line, token = tokens[idx]
+        start: int = idx
+
+        if token in type_tokens:  # Type
+            typ: Type = Type(qualifiers=[], type=TypeType[token], pdepth=0)
+
+            if tokens[idx + 1][2] == "*":  # Parse pointer depth
+                idx += 1  # Skip the type
+
+                while tokens[idx][2] == "*":
+                    typ.pdepth += 1
+                    idx += 1
+
+            ast.append(
+                Token(
+                    type=TokenType.type,
+                    token=token,
+                    char=char,
+                    line=line,
+                    info=typ,
+                )
+            )
+        elif token == "(": # Function definition
+            fatal(file, char, line, f"Todo: {token!r}")
+        else:
+            if ast and ast[-1].type == TokenType.type:
+                typ_tok: Token = ast.pop()
+
+                if typ_tok.info is None:
+                    warn(file, typ_tok.line, typ_tok.char, "Type has no type attribute.")
+                    fatal(file, char, line, f"Unknown type for {token!r}")
+
+                ast.append(
+                    Token(
+                        type=TokenType.aname,
+                        token=token,
+                        char=char,
+                        line=line,
+                        info=Name(type=typ_tok.info, name=token),
+                    )
+                )
+            else:
+                print(ast)
+                fatal(file, char, line, f"Unknown token {token!r}")
+
+        idx += 1
+
+    return ast
 
 
 def main() -> int:
@@ -156,8 +309,30 @@ def main() -> int:
         print("Please supply the Kx file to compile.", file=sys.stderr)
         return 1
 
-    with open(sys.argv[1], "r") as fp:
-        print(tokenize(fp.read()))
+    file: str = sys.argv[1]
+
+    info(file, 0, 0, f"Tokenizing {file} ...")
+
+    lines, chars, tokens = 0, 0, []
+
+    with open(file, "r") as fp:
+        lines, chars, tokens = tokenize(fp.read())
+
+    if len(tokens) == 0:
+        fatal(file, 0, 0, "No tokens found.")
+
+    print(tokens)
+    success(
+        file,
+        lines,
+        chars,
+        f"Found {len(tokens)} tokens - processed {lines} line(s) and {chars} character(s). Building an AST ...",
+    )
+
+    ast: t.List[Token] = build_ast(tokens, sys.argv[1])
+
+    print(ast)
+    success(file, line, char, f"Found {len(ast)} ast element(s).")
 
     return 0
 
